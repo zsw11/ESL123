8581e0f86463e58809bee889133466ee6e096a86
RoyLuo <RoyLuo@apjcorp.com>
2019-11-18 09:15:42 +0800

    update

---
+++ backend/ESL/src/main/java/io/apj/modules/masterData/dao/ActionDao.java
@@ -0,0 +1,17 @@
+package io.apj.modules.masterData.dao;
+
+import io.apj.modules.masterData.entity.ActionEntity;
+import org.apache.ibatis.annotations.Mapper;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * 关键词
+ * 
+ * @author RoyLuo
+ * @email RoyLuo@apjcorp.com
+ * @date 2019-11-07 10:39:25
+ */
+@Mapper
+public interface ActionDao extends BaseMapper<ActionEntity> {
+	
+}
