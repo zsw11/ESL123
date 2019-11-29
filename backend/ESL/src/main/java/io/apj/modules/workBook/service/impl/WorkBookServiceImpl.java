90a768ea1ac856dd2fbf0821f51e47ed64043e66
shuwen123 <546232194@qq.com>
2019-11-29 13:28:49 +0800

    分析表搜索

--- backend/ESL/src/main/java/io/apj/modules/workBook/service/impl/WorkBookServiceImpl.java
+++ backend/ESL/src/main/java/io/apj/modules/workBook/service/impl/WorkBookServiceImpl.java
@@ -1,6 +1,7 @@
 package io.apj.modules.workBook.service.impl;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.toolkit.StringUtils;
 import io.apj.modules.masterData.entity.ModelEntity;
 import io.apj.modules.masterData.entity.ToolEntity;
 import io.apj.modules.masterData.service.ModelService;
@@ -37,9 +38,23 @@ public class WorkBookServiceImpl extends ServiceImpl<WorkBookDao, WorkBookEntity
         EntityWrapper<WorkBookEntity> entityWrapper = new EntityWrapper<>();
         entityWrapper.isNull("delete_at")
                 .like(params.get("keyWord") != null && params.get("keyWord") != "", "destinations", (String) params.get("keyWord"))
-                .like(params.get("workName") != null && params.get("workName") != "", "work_name", (String) params.get("workName"))
-                .like(params.get("makerId") != null && params.get("makerId") != "", "maker_id", (String) params.get("makerId"))
-        ;
+                .like(params.get("workName") != null && params.get("workName") != "", "work_name", (String) params.get("workName"));
+        if(StringUtils.isNotEmpty((CharSequence) params.get("makerId"))){
+            entityWrapper.eq("maker_id",Integer.parseInt((String) params.get("makerId")));
+        }
+        if(StringUtils.isNotEmpty((CharSequence) params.get("deptId"))){
+            entityWrapper.eq( "dept_id", Integer.parseInt((String) params.get("deptId")));
+        }
+        if(StringUtils.isNotEmpty((CharSequence) params.get("modelId"))){
+            entityWrapper.eq("model_id", Integer.parseInt((String) params.get("modelId")));
+        }
+        if(StringUtils.isNotEmpty((CharSequence) params.get("phaseId"))){
+            entityWrapper.eq( "phase_id",Integer.parseInt((String) params.get("phaseId")));
+        }
+        if(StringUtils.isNotEmpty((CharSequence) params.get("workstationId"))){
+            entityWrapper.eq("workstation_id", Integer.parseInt((String) params.get("workstationId")));
+        }
+
         Page<WorkBookEntity> page = this.selectPage(new Query<WorkBookEntity>(params).getPage(), entityWrapper);
         for (WorkBookEntity entity : page.getRecords()) {
             if(entity.getDeptId()!=null){
