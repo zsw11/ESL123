7cca22f8602707ee33a95cee1b3b7e7bbd5c7978
BOS-markmajiansheng <13411443538@163.com>
2019-12-27 17:39:21 +0800

    1.添加下载方法

--- electron/src/renderer/views/modules/report/stationtime.vue
+++ electron/src/renderer/views/modules/report/stationtime.vue
@@ -247,6 +247,7 @@
 <el-button
               size="mini"
               type="text"
+              @click="down(scope.row)"
             >下载</el-button>
             <el-button
               size="mini"
@@ -312,7 +313,7 @@ import { listPhase } from '@/api/phase'
 import { fetchReportGroup } from '@/api/report'
 import { keyBy } from 'lodash'
 import { listDict, listDictItem } from '@/api/dict'
-import { createReportApprove } from '@/api/reportApprove'
+import { createReportApprove, downloadReportApprove } from '@/api/reportApprove'
 import { listStaff } from '@/api/staff'
 
 export default {
@@ -614,6 +615,17 @@ export default {
       listDictItem({ type: 'ST' }).then(({data}) => {
         this.dictItemSTLST = keyBy(data, 'code')
       })
+    },
+    down(row){
+      let data ={
+        modelId: row.modelId,
+        phaseId: row.phaseId,
+        stlst: row.stlst,
+        reportId: 3
+      }
+      downloadReportApprove(data).then(response => {
+        
+      });
     }
   }
 }
