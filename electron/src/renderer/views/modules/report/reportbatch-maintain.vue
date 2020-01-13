6cb5b8f357bb2b11ee1c97f3189c45522cb50746
XiongChi <1076117890@qq.com>
2020-01-13 18:00:42 +0800

    报表批次

--- electron/src/renderer/views/modules/report/reportbatch-maintain.vue
+++ electron/src/renderer/views/modules/report/reportbatch-maintain.vue
@@ -2,119 +2,123 @@
 <template>
   <el-card class="with-title">
     <div slot="header" class="clearfix">
-      <div class="card-title">ReportChangeRecord</div>
+      <div class="card-title">{{title}}</div>
       <div class="buttons">
-        <el-button type="primary" @click="dataFormSubmit()">保 存</el-button>
-        <el-button @click="cancleFormSubmit">取 消</el-button>
+<!--        <el-button type="primary" @click="dataFormSubmit()">保 存</el-button>-->
+<!--        <el-button @click="cancleFormSubmit">取 消</el-button>-->
+        <el-button v-if="$route.path.includes('details')" @click="cancleFormSubmit">确 定</el-button>
       </div>
     </div>
     <el-form
+      :disabled="!$route.path.includes('createreport')"
       :rules="dataRules"
       ref="dataForm"
       :model="dataForm"
       label-position="right"
       :size="'mini'"
       label-width="100px"
-      style="width: 95%"
-    >
-      <el-row>
+      style="width: 95%">
+
+      <el-row :gutter="10">
         <el-col :span="10">
-          <el-form-item :label="'Sheet名称'" prop="sheetName">
-            <el-input v-model="dataForm.sheetName"></el-input>
-          </el-form-item>
-        </el-col>
-
-        <el-col :span="10" :offset="2">
-          <el-form-item :label="'机种'" prop="modelName">
-            <el-input :disabled="true" v-model="dataForm.modelName"></el-input>
-          </el-form-item>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="10">
-          <el-form-item :label="'生产阶段'" prop="phaseName">
-            <el-input :disabled="true" v-model="dataForm.phaseName"></el-input>
-          </el-form-item>
-        </el-col>
-
-        <el-col :span="10" :offset="2">
-          <el-form-item :label="'ST/LST'" prop="stlst">
-            <dict-select dictType="ST" style="width:100%" :disabled="true" v-model="dataForm.stlst"></dict-select>
-          </el-form-item>
-        </el-col>
-      </el-row>
-
-
-
-      <el-row>
-        <el-col :span="10">
-          <el-form-item :label="'型号'" prop="modelType">
-            <el-input v-model="dataForm.modelType"></el-input>
+          <el-form-item :label="'机种'" prop="modelId">
+            <keyword-search
+              style="width: 100%"
+              v-model="dataForm.modelId"
+              :searchApi="this.listModel"
+              :allowEmpty="true"
+              :defaultOptions="defaultModel">
+            </keyword-search>
           </el-form-item>
         </el-col>
         <el-col :span="10" :offset="2">
           <el-form-item :label="'仕向'" prop="destinations">
-            <el-input v-model="dataForm.destinations"></el-input>
+            <el-input
+              v-model="dataForm.destinations">
+            </el-input>
           </el-form-item>
         </el-col>
       </el-row>
-
-      <el-row>
+      <el-row :gutter="10">
         <el-col :span="10">
-          <el-form-item :label="'工场'" prop="factory">
-            <el-input v-model="dataForm.factory"></el-input>
+          <el-form-item :label="'LST/ST'" prop="stlst">
+            <dict-select
+              style="width: 100%"
+              dictType="ST"
+              v-model="dataForm.stlst">
+            </dict-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="10" :offset="2">
+          <el-form-item :label="'生产阶段'" prop="phaseId">
+            <keyword-search
+              style="width: 100%"
+              v-model="dataForm.phaseId"
+              :searchApi="this.listPhase"
+              :allowEmpty="true">
+            </keyword-search>
           </el-form-item>
         </el-col>
       </el-row>
 
-
-
-      <el-button @click="addItem()">加记录</el-button>
-      <el-row>
-        <el-table
-          :data="dataForm.items"
-          style="width: 100%;"
-        >
-          <el-table-column align="center" prop="makedAt" label="制定日期">
-            <template slot-scope="scope">
-              <el-input type="date" v-model="scope.row.makedAt" />
-            </template>
-          </el-table-column>
-
-          <el-table-column align="center" prop="processName" label="工程名">
-            <template slot-scope="scope">
-              <el-input type="text" v-model="scope.row.processName" />
-            </template>
-          </el-table-column>
-
-          <el-table-column align="center" prop="content" label="修改内容">
-            <template slot-scope="scope">
-              <el-input type="text" v-model="scope.row.content" />
-            </template>
-          </el-table-column>
-
-          <el-table-column align="center" prop="currentValue" label="当前耗时值" >
-            <template slot-scope="scope">
-              <el-input type="text" v-model="scope.row.currentValue" />
-            </template>
-          </el-table-column>
-
-          <el-table-column align="center" prop="lastValue" label="变更前耗时值">
-            <template slot-scope="scope">
-              <el-input type="text" v-model="scope.row.lastValue" />
-            </template>
-          </el-table-column>
-        </el-table>
+      <el-row :gutter="10">
+        <el-col :span="10">
+          <el-form-item :label="'版本号'" prop="versionNumber">
+            <el-input v-model="dataForm.versionNumber"></el-input>
+          </el-form-item>
+        </el-col>
       </el-row>
 
     </el-form>
 
+    <el-dialog
+      customClass="wookbook-dialog"
+      width="30%"
+      title="生成报表"
+      :visible.sync="createShow">
 
+<!--      <div class="dialog-block">-->
+<!--        <h4>确定读取{{number}}张分析表</h4>-->
+<!--        <h4>生成</h4>-->
+<!--        <el-row>-->
+<!--          <el-col :span="10">-->
+<!--            <span>机种: {{dataForm.model}}</span>-->
+<!--          </el-col>-->
+<!--          <el-col :span="10" :offset="2">-->
+<!--            <span>仕向: {{dataForm.destinations}}</span>-->
+<!--          </el-col>-->
+<!--        </el-row>-->
+<!--        <el-row :gutter="2">-->
+<!--          <el-col :span="10">-->
+<!--            <span>生产阶段: {{dataForm.phase}}</span>-->
+<!--          </el-col>-->
+<!--          <el-col :span="10" :offset="2">-->
+<!--            <span>ST/LST: {{dataForm.stlst}}</span>-->
+<!--          </el-col>-->
+<!--        </el-row>-->
+<!--        <el-row :gutter="2">-->
+<!--          <el-col :span="10">-->
+<!--            <span>版本号: {{dataForm.versionNumber}}</span>-->
+<!--          </el-col>-->
+<!--        </el-row>-->
+<!--      </div>-->
+      <div class="dialog-block">
+<!--        <h4>如下报表:</h4>-->
+        <h4>确定读取{{number}}张分析表生成</h4>
+        <span v-for="item in reportGroup" class="report"
+              :key="item.id">{{item.name}}</span>
+      </div>
+      <span slot="footer" class="dialog-footer">
+            <el-button @click="createShow = false">取 消</el-button>
+            <el-button type="primary" @click="createReportOK">确 定</el-button>
+          </span>
+    </el-dialog>
 
     <span class="dialog-footer">
-      <el-button type="primary" @click="dataFormSubmit()">保 存</el-button>
-      <el-button @click="cancleFormSubmit">取 消</el-button>
+      <el-button v-if="$route.path.includes('createreport')" type="primary" @click="createReport">生 成</el-button>
+<!--      <el-button type="primary" @click="dataFormSubmit()">保 存</el-button>-->
+      <el-button v-if="$route.path.includes('createreport')" @click="cancleFormSubmit">取 消</el-button>
+      <el-button v-if="$route.path.includes('details')" @click="cancleFormSubmit">确 定</el-button>
     </span>
   </el-card>
 </template>
@@ -124,26 +128,33 @@
   import {
     fetchReportBatch,
     createReportBatch,
-    updateReportBatch
+    updateReportBatch,
+    createReports
   } from '@/api/reportBatch'
+  import { listPhase } from '@/api/phase'
+  import { listModel } from '@/api/model'
+  import { fetchDeptReport } from '@/api/workBook'
+
   export default {
     name: 'editReportBatch',
     data () {
       return {
+        title: null,
         inited: false,
+        number: null,
+        createShow: false,
+        createForm: {
+          id: []
+        },
+        reportGroup: [],
+        model: null,
         dataForm: {
           id: 0,
-          deptId: null,
-          title: null,
-          sheetName: null,
-          factory: null,
-          modelId: null,
-          phaseId: null,
-          modelName: null,
-          phaseName: null,
           stlst: null,
-          modelType: null,
+          modelId: null,
           destinations: null,
+          phaseId: null,
+          versionNumber: null,
           createBy: null,
           createAt: null,
           updateBy: null,
@@ -156,13 +167,19 @@
           title: [{ max: 128, message: '长度超过了128', trigger: 'blur' }],
           sheetName: [{ max: 128, message: '长度超过了128', trigger: 'blur' }],
           factory: [{ max: 64, message: '长度超过了64', trigger: 'blur' }],
-          modelId: [{ type: 'number', message: '机种ID需为数字值' }],
+          modelId: [{required: true, message: '机种不能为空' }],
+          phaseId: [{required: true, message: '生产阶段不能为空' }],
+          stlst: [{required: true, message: 'ST/LST不能为空' }],
           modelType: [{ max: 64, message: '长度超过了64', trigger: 'blur' }],
-          destinations: [{ max: 64, message: '长度超过了64', trigger: 'blur' }],
+          destinations: [{required: true, message: '仕向不能为空' }],
+          versionNumber: [{required: true, message: '版本号不能为空' }],
           createBy: [{ type: 'number', message: '创建者ID需为数字值' }],
           updateBy: [{ type: 'number', message: '更新者ID需为数字值' }]
         },
-        dataList: []
+        listPhase,
+        listModel,
+        dataList: [],
+        defaultModel:[]
       }
     },
     beforeRouteEnter (to, from, next) {
@@ -196,6 +213,7 @@
     },
     methods: {
       init () {
+        this.title = this.$route.meta.title
         this.$store.dispatch('common/updateTabAttrs', {
           name: this.$route.name,
           changed: false
@@ -204,33 +222,24 @@
         this.dataForm.id = parseInt(this.$route.params.id) || 0
         if (this.dataForm.id) {//this.dataForm.id
           fetchReportBatch(this.dataForm.id)
-            .then((data ) => {
+            .then((data) => {
               Object.assign(
                 this.dataForm,
-                pick(data, [
+                pick(data[0], [
                   'id',
-                  'deptId',
-                  'title',
-                  'sheetName',
                   'modelId',
                   'phaseId',
-                  'modelName',
-                  'phaseName',
                   'stlst',
-                  'factory',
-                  'modelType',
                   'destinations',
+                  'versionNumber',
                   'createBy',
                   'createAt',
                   'updateBy',
                   'updateAt',
                   'deleteAt',
-                  'items'
                 ])
               )
-              this.dataList = data.items||[]
-              console.log(this.dataForm)
-              console.log(this.dataList)
+              this.defaultModel = [data[0].modelEntity]
             })
             .finally(() => {
               this.inited = true
@@ -263,9 +272,55 @@
           }
         })
       },
-      addItem(){
-        this.dataForm.items.push({'maked_at':new Date(),'reportChangeRecordId':this.dataForm.id})
+      // 生成报表
+      createReport () {
+        this.$refs['dataForm'].validate(valid => {
+          if(valid){
+            this.createForm.id = []
+            this.reportGroup = []
+            fetchDeptReport().then((page)=>{
+              this.reportGroup = page.data
+              this.reportGroup.forEach((item)=>{
+                this.createForm.id.push(item.id)
+              })
+            })
+            this.createShow = true
+          }
+        })
+      },
+      // 确定生成
+      createReportOK (row) {
+        console.log(this.createForm.id, this.dataForm)
+        createReports(Object.assign(
+          {
+            workBook: this.dataForm,
+            reports: this.createForm.id
+          }
+        )).then((page) => {
+          if(page.msg === 'success'){
+            this.$message({
+              message: '操作成功',
+              type: 'success',
+              duration: 1500,
+            })
+          }
+          this.createForm.id = []
+          this.createShow = false
+        }).catch(() => {
+        }).finally(() => {
+        })
       }
     }
   }
 </script>
+<style lang="scss" scoped>
+  .wookbook-dialog {
+    min-width: 400px;
+  }
+
+  .report{
+    display: inline-block;
+    padding-left: 5px;
+    width: 50%;
+  }
+</style>
